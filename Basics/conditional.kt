ec7b9ce7861812bdd674473864fffb3dae12f7c2
PratickB111 <animeNerd.int@protonmail.com>
2024-03-01 23:44:11 +0530

    added some more code in conditional kotlin

--- Basics/conditional.kt
+++ Basics/conditional.kt
@@ -13,12 +13,33 @@ fun main() {
       print("Grade: B")
     } else if(firstStudentMarks < 80 && firstStudentMarks > 70) {
       print("Grade: C")
-    } else if(!(firstStudentMarks <= 100 && firstStudentMarks >= 1)) {
-      print("Marks should be between 1 to 100")
+    } else if(firstStudentMarks < 60 && firstStudentMarks > 70) {
+      print("Grade: D")
     } else {
-      print("Grade: C")
+      print("Marks should be between 1 to 100")
     }
   } else {
     print("Please provide marks")
   }
+
+  // using if-else block to find grades using ranges: 'in'
+  var secStudentMarks: Int? = null
+
+  if(secStudentMarks != null) {
+    if(secStudentMarks > 90) {
+      print("Grade: A")
+    } else if(secStudentMarks in 80..90) {
+      print("Grade: B")
+    } else if(secStudentMarks in 70..80) {
+      print("Grade: C")
+    } else if(secStudentMarks in 60..70) {
+      print("Grade: D")
+    } else {
+      print("Marks should be between 1 to 100")
+    }
+  } else {
+    print("Please provide marks")
+  }
+
+  
 }
