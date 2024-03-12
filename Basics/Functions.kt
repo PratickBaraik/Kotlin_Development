36da39f19171b8d1fa3e10bb53a7da4c587a6eda
PratickB111 <animeNerd.int@protonmail.com>
2024-03-12 23:23:55 +0530

    adding greeting function block

--- Basics/Functions.kt
+++ Basics/Functions.kt
@@ -1,8 +1,14 @@
 fun main() {
   sayHello();
+  myGreeting("Pratick");
 }
 
 // sayHello() function definition
 fun sayHello() {
-  print("Hello");
+  print("Hello\n");
+}
+
+// my greeting function definition
+fun myGreeting(name: String) {
+  print("Hello $name\n");
 }
