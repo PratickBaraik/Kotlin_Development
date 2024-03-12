37ccc78521e017caea07b68f0d9765c1c3e74524
PratickB111 <animeNerd.int@protonmail.com>
2024-03-12 23:40:10 +0530

    adding vararg type function parameter

--- Basics/Functions.kt
+++ Basics/Functions.kt
@@ -13,12 +13,12 @@ fun sayHello() {
 
 // my greeting function definition
 fun myGreeting(name: String) {
-  println("Function which takes list of string values-");
+  println("Function which is printing a string passed in function argument-");
   println("Hello $name");
 }
 
 fun listGreeting(names: List<String>) {
-  println("Function which takes list of any values-");
+  println("Function which takes list of string values-");
   names.forEach {
     println("Hello, $it")
   }
@@ -26,7 +26,16 @@ fun listGreeting(names: List<String>) {
 
 // function which can take any type of values in list
 fun anyList(values: List<Any>) {
+  println("Function which takes list of any values-");
   values.forEach {
+    println("$it");
+  }
+}
+
+// using vararg as parameter in function
+fun myVarArgFun(vararg items: String) {
+  println("Function which takes vararg parameter-");
+  items.forEach {
     println("$it");
   }
 }
