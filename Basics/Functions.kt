9a8b6d921848c1d56ad7a0ab8dae12654e0d79fd
PratickB111 <animeNerd.int@protonmail.com>
2024-03-12 23:36:59 +0530

    adding function block which accepts list of any type of value

--- Basics/Functions.kt
+++ Basics/Functions.kt
@@ -2,20 +2,31 @@ fun main() {
   sayHello();
   myGreeting("Pratick");
   listGreeting(listOf("Sujal", "Ranu", "Satyam", "Mohit", "Jeetu"));  
+  anyList(listOf("String", true, 123, 117893245, 88.43, 'A'));
 }
 
 // sayHello() function definition
 fun sayHello() {
+  println("Function which is printing a simple message-");
   println("Hello");
 }
 
 // my greeting function definition
 fun myGreeting(name: String) {
+  println("Function which takes list of string values-");
   println("Hello $name");
 }
 
 fun listGreeting(names: List<String>) {
-    names.forEach {
-        println("Hello, $it")
-    }
+  println("Function which takes list of any values-");
+  names.forEach {
+    println("Hello, $it")
+  }
+}
+
+// function which can take any type of values in list
+fun anyList(values: List<Any>) {
+  values.forEach {
+    println("$it");
+  }
 }
