6608b96db877f4ac6fa88585c399703a14b055eb
PratickB111 <animeNerd.int@protonmail.com>
2024-03-16 19:42:12 +0530

    adding file for simple array using kotlin

---
+++ collections/SimpleArray.kt
@@ -0,0 +1,10 @@
+fun main() {
+  // defining a simple array in kotlin
+  val myArray: Array<String> = arrayOf("Pratick", "Atul", "Vicky", "Vishal", "Sagar")
+  // printing the elements of array turn by turn
+  println("1st element: ${myArray[0]}");
+  println("2nd element: ${myArray[1]}");
+  println("3rd element: ${myArray[2]}");
+  println("4th element: ${myArray[3]}");
+  println("5th element: ${myArray[4]}");
+}
