4ee2c7e627a2c5c136a1a911f253cb2b8881b0e9
PratickB111 <animeNerd.int@protonmail.com>
2024-03-17 20:00:10 +0530

    changed the code of kotlin map file

--- collections/MapKotlin.kt
+++ collections/MapKotlin.kt
@@ -1,10 +1,30 @@
-// kotlin map is a collection which stores the any item or value into a key value pair
-// each key is unique and map holds only one value for each key
-// starting the program
-fun main() { // driver code
-  // defining a simple map
-  val myMap = mapOf("Calculator" to 1, "Calendar" to 2, "Laptop" to 3, "Notebook" to 4);
-  // printing the kotlin map
-  println("Map items: ")
-  println(myMap)
+// Defining a function to print values of a map
+fun printMap(myMap: Map<String, Int>) {
+    println("Map items:")
+    myMap.forEach { (key, value) ->
+        println("$key -> $value")
+    }
+}
+
+fun main() {
+    // Defining a simple map which is immutable
+    val myMap = mapOf("Calculator" to 1, "Calendar" to 2, "Laptop" to 3, "Notebook" to 4)
+    // Printing the Kotlin map
+    println("Immutable map:")
+    printMap(myMap)
+
+    // Defining a mutable map in Kotlin
+    val myMutableMap = mutableMapOf("Table" to 10, "Chair" to 20, "Bed" to 30, "Wardrobe" to 40, "Dressing Table" to 50)
+    // Printing the mutable map
+    println("\nMutable map:")
+    printMap(myMutableMap)
+
+    // Adding values to the mutable map
+    myMutableMap["Door"] = 11
+    myMutableMap["Window"] = 12
+    myMutableMap["Balcony"] = 13
+
+    // Printing the updated mutable map
+    println("\nUpdated mutable map:")
+    printMap(myMutableMap)
 }
