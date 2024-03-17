d3d6fd6dd7baa3447e88ea7a418d1c938a454361
PratickB111 <animeNerd.int@protonmail.com>
2024-03-17 16:33:34 +0530

    adding the set file of kotlin

---
+++ collections/SetKotlin.kt
@@ -0,0 +1,14 @@
+// set is a collection in kotlin which contains unique values
+// starting the program
+fun main() { // driver code
+
+
+}
+
+// defining a function to print set items
+fun print_set() {
+  println("\nPrinting the set items: ")
+  mySet.forEach{
+    items -> print("$items  ");
+  }
+}
