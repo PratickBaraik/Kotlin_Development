94cd7ef88da6aaa124adece8c1b66fa162c025f9
PratickB111 <animeNerd.int@protonmail.com>
2024-03-17 19:05:30 +0530

    adding codes in kotlin set file

--- collections/SetKotlin.kt
+++ collections/SetKotlin.kt
@@ -1,14 +1,16 @@
 // set is a collection in kotlin which contains unique values
 // starting the program
 fun main() { // driver code
-
+  val mySet = setOf("Ruler", "Calendar", "Notebook", "Id Card", "Ruler", "Pencil", "Pencil");
+  // printing the set items
+  print("Set items: $mySet");
 
 }
 
-// defining a function to print set items
-fun print_set() {
-  println("\nPrinting the set items: ")
-  mySet.forEach{
-    items -> print("$items  ");
-  }
-}
+// // defining a function to print set items
+// fun print_set() {
+//   println("\nPrinting the set items: ")
+//   mySet.forEach{
+//     items -> print("$items  ");
+//   }
+// }
