054fce55747fb3a44ddbdfc64c1439f55b68970f
PratickB111 <animeNerd.int@protonmail.com>
2024-03-17 19:14:02 +0530

    adding some code in kotlin set file

--- collections/SetKotlin.kt
+++ collections/SetKotlin.kt
@@ -5,6 +5,15 @@ fun main() { // driver code
   // printing the set items
   print("Set items: $mySet");
 
+  val numberSetOne = setOf(1, 2, 3, 4, 5);
+  val numberSetTwo = setOf(2, 4, 5, 6, 8);
+  // performing union operation on set
+  // printing the union of two set
+  println("\nUnion: ${numberSetOne union numberSetTwo}");
+
+  // performing intersection on two sets
+  println("\nIntersection: ${numberSetOne intersect numberSetTwo}");
+
 }
 
 // // defining a function to print set items
