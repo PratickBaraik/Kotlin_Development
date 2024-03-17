7ad780b66ddc21d21844fceeef6f93266d452937
PratickB111 <animeNerd.int@protonmail.com>
2024-03-17 19:19:09 +0530

    adding kotlin map file

---
+++ collections/MapKotlin.kt
@@ -0,0 +1,10 @@
+// kotlin map is a collection which stores the any item or value into a key value pair
+// each key is unique and map holds only one value for each key
+// starting the program
+fun main() { // driver code
+  // defining a simple map
+  val myMap = mapOf("Calculator" to 1, "Calendar" to 2, "Laptop" to 3, "Notebook" to 4);
+  // printing the kotlin map
+  println("Map items: ")
+  println(myMap)
+}
